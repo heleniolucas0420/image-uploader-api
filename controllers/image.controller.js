e8d04c2249210e4c424633b9c0992fb7f5e298c3
Helenio Lucas <heleniolucas@Helenios-MacBook-Air.local>
2022-09-27 20:24:47 +0200

    returning right link on response ☃️

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,23 +1,31 @@
 const Image = require('../models/image.model');
-const { getImageReference, getImageExtension } = require('../utils/image.utils');
-
+const {
+  getImageReference,
+  getImageExtension,
+} = require('../utils/image.utils');
 
 exports.postImageUpload = (req, res, next) => {
-  if (!req.file) res.status(400).json({ 
-    success: false, 
-    message: 'You fucked up. Send a valid file Format!' 
-  });
+  if (!req.file)
+    res.status(400).json({
+      success: false,
+      message: 'You fucked up. Send a valid file Format!',
+    });
 
   const { filename } = req.file;
   const image_reference = getImageReference(filename);
-  
-  const image = new Image(image_reference, filename, `http://localhost:4000/images/${filename}`);
+
+  const url =
+    process.env.NODE_ENV === 'development'
+      ? `http://localhost:4000/images/${filename}`
+      : `https://murmuring-waters-99242.herokuapp.com/images/${filename}`;
+
+  const image = new Image(image_reference, filename, url);
   image.save();
 
-  res.status(200).json({ 
-    success: true, 
+  res.status(200).json({
+    success: true,
     message: 'Image uploaded succesfully!',
-    response_data: image
+    response_data: image,
   });
 };
 
@@ -33,5 +41,4 @@ exports.getImage = (req, res, next) => {
       res.send(image);
     }
   });
-
-}
+};
